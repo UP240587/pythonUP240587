8b4e6529853da4f24ddf9ea93391c64503dc11c2
ozgam <up240587@alumnos.upa.edu.mx>
2025-06-05 10:38:18 -0600

    day2_ejercicio

---
+++ Day1_L1yL2/D1_L1.py
@@ -0,0 +1,37 @@
+print(type(565447))
+
+print(5+5)
+
+print(5-5)
+
+print(5*5)
+
+print(5/5)
+
+print(5%100)
+
+print(6**2)
+
+print(7//2)
+
+print('Osiel Josafat Reyes Herrera')
+
+print('Family Reyes Herrea')
+
+print('My ciudad es aguascalientes')
+
+print('I am enjoying 30 days of python')
+
+print(type(10)) 
+
+print(type(9.8)) 
+
+print(type(3.14)) 
+
+print(type['Asabeneh', 'Python', 'Finland']) 
+
+print(type('Osiel Josafat Reyes Herrera'))
+
+print(type('Family Reyes Herrea'))
+
+print(type('My ciudad es aguascalientes'))
